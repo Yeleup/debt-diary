2709829ed01cd52f954abe1287fac2d203a6cfa6
Magzhan Yeleup <magzhan9292@gmail.com>
2023-10-17 09:32:50 +0600

    Small fix

--- src/DataFixtures/TypeFixtures.php
+++ src/DataFixtures/TypeFixtures.php
@@ -14,18 +14,21 @@ class TypeFixtures extends Fixture
         $type->setTitle('Приход');
         $type->setPrefix('-');
         $type->setPaymentStatus(1);
+        $type->setColor('#0ba300');
         $manager->persist($type);
 
         $type = new Type();
         $type->setTitle('Реализация');
         $type->setPrefix('+');
         $type->setPaymentStatus(0);
+        $type->setColor('#0052d6');
         $manager->persist($type);
 
         $type = new Type();
         $type->setTitle('Возврат');
         $type->setPrefix('-');
         $type->setPaymentStatus(0);
+        $type->setColor('#a8a8a8');
         $manager->persist($type);
 
         $manager->flush();
