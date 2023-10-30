c756ad18b4bcda63faeede3ec0f95977f6107b89
Magzhan Yeleup <magzhan9292@gmail.com>
2023-10-30 10:52:49 +0600

    Переделали статистику

--- src/Controller/Api/Action/GetTransactionStatistic.php
+++ src/Controller/Api/Action/GetTransactionStatistic.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Api\Action;
 
 use App\Entity\Transaction;
-use App\Entity\User;
 use App\Repository\MarketRepository;
 use App\Repository\PaymentRepository;
 use App\Repository\TypeRepository;
@@ -16,7 +15,7 @@ use Symfony\Component\Security\Core\Security;
 
 class GetTransactionStatistic
 {
-    private User $user;
+    private Security $security;
     private TypeRepository $typeRepository;
     private MarketRepository $marketRepository;
     private PaymentRepository $paymentRepository;
@@ -33,7 +32,7 @@ class GetTransactionStatistic
         $this->marketRepository = $marketRepository;
         $this->moneyFormatter = $moneyFormatter;
         $this->paymentRepository = $paymentRepository;
-        $this->user = $security->getUser();
+        $this->security = $security;
     }
 
     /**
@@ -46,48 +45,67 @@ class GetTransactionStatistic
      */
     public function __invoke(Request $request): JsonResponse
     {
-        $endDate = Carbon::createFromFormat('Y-m-d', $request->query->get('endDate'))->endOfDay();
+        $startDate = null;
+        if ($request->query->get('startDate')) {
+            $startDate = Carbon::createFromFormat('Y-m-d', $request->query->get('startDate'))->startOfDay();
+        }
+
+        $endDate = null;
+        if ($request->query->get('endDate')) {
+            $endDate = Carbon::createFromFormat('Y-m-d', $request->query->get('endDate'))->endOfDay();
+        }
 
         $markets = $this->marketRepository->createQueryBuilder('m')
             ->join('m.users', 'u')
-            ->where('u.id = :user')
-            ->setParameter('user', $this->user)
+            ->where('u.username = :userIdentifier')
+            ->setParameter('userIdentifier', $this->security->getUser()->getUserIdentifier())
             ->getQuery()
             ->getResult()
         ;
 
         $data = [];
         foreach ($markets as $market) {
-            $types = $this->typeRepository->createQueryBuilder('t')
-                ->select('t.id', 't.title', 't.payment_status', 'SUM(transaction.amount) as value')
+            $qb = $this->typeRepository->createQueryBuilder('t');
+            $qb->select('t.id', 't.title', 't.payment_status', 'SUM(transaction.amount) as value')
                 ->join('t.transactions', 'transaction')
                 ->join('transaction.customer', 'customer')
                 ->join('customer.market', 'market')
-                ->where('transaction.createdAt BETWEEN :startDate AND :endDate AND market.id = :market_id')
-                ->groupBy('t.id')
-                ->setParameter('startDate', $request->query->get('startDate'))
-                ->setParameter('endDate', $endDate)
-                ->setParameter('market_id', $market->getId())
-                ->getQuery()
-                ->getResult();
+                ->where('market.id = :market_id')
+                ->setParameter('market_id', $market->getId());
+
+            if ($startDate) {
+                $qb->andWhere('transaction.createdAt > :startDate')->setParameter('startDate', $startDate);
+            }
+
+            if ($endDate) {
+                $qb->andWhere('transaction.createdAt < :endDate')->setParameter('endDate', $endDate);
+            }
+
+            $types = $qb->groupBy('t.id')->getQuery()->getResult();
 
             $statistics = [];
             $value = 0;
             foreach ($types as $type) {
-                $payments = $this->paymentRepository->createQueryBuilder('p')
-                    ->select('p.id', 'p.title', 'SUM(transaction.amount) as value')
+                $qb = $this->typeRepository->createQueryBuilder('p');
+                $qb->select('p.id', 'p.title', 'SUM(transaction.amount) as value')
                     ->join('p.transactions', 'transaction')
                     ->join('transaction.type', 'type')
                     ->join('transaction.customer', 'customer')
                     ->join('customer.market', 'market')
-                    ->where('type.id = :typeId AND transaction.createdAt BETWEEN :startDate AND :endDate AND market.id = :market_id')
+                    ->where('type.id = :typeId')
+                    ->andWhere('market.id = :marketId')
                     ->setParameter('typeId', $type['id'])
-                    ->setParameter('startDate', $request->query->get('startDate'))
-                    ->setParameter('endDate', $endDate)
-                    ->setParameter('market_id', $market->getId())
-                    ->groupBy('p.id')
-                    ->getQuery()
-                    ->getResult();
+                    ->setParameter('marketId', $market->getId());
+
+                if ($startDate) {
+                    $qb->andWhere('transaction.createdAt > :startDate')->setParameter('startDate', $startDate);
+                }
+
+                if ($endDate) {
+                    $qb->andWhere('transaction.createdAt < :endDate')->setParameter('endDate', $endDate);
+                }
+
+                $payments = $qb->groupBy('p.id')->getQuery()->getResult();
 
                 $statisticPayments = [];
                 foreach ($payments as $payment) {
