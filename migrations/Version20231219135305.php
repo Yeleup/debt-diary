850f9f205df43a243a9f13cd210c76640c775c86
Magzhan Yeleup <magzhan9292@gmail.com>
2023-12-20 15:18:38 +0600

    Correct migrations

--- migrations/Version20231219135305.php
+++ migrations/Version20231219135305.php
@@ -20,7 +20,7 @@ final class Version20231219135305 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE expense_type ADD is_add_expense TINYINT(1) NOT NULL DEFAULT 1');
+        $this->addSql('ALTER TABLE expense_type ADD is_add_expense TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
