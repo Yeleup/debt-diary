55763891eb219b4fe399038428db0340c0dbf5b2
Magzhan Yeleup <magzhan9292@gmail.com>
2024-05-18 06:52:52 +0500

    Delete user processor

--- src/State/UserStateProcessor.php
+++ src/State/UserStateProcessor.php
@@ -2,19 +2,27 @@
 
 namespace App\State;
 
+use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserStateProcessor implements ProcessorInterface
 {
-    public function __construct(protected ProcessorInterface $decorated, protected UserPasswordHasherInterface $passwordHasher)
+    public function __construct(
+        protected ProcessorInterface $persistProcessor,
+        protected ProcessorInterface $removeProcessor,
+        protected UserPasswordHasherInterface $passwordHasher
+    )
     {
     }
 
     public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
     {
+        if ($operation instanceof DeleteOperationInterface) {
+            return $this->removeProcessor->process($data, $operation, $uriVariables, $context);
+        }
         $data->setPassword($this->passwordHasher->hashPassword($data, $data->getPassword()));
-        return $this->decorated->process($data, $operation, $uriVariables, $context);
+        return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
